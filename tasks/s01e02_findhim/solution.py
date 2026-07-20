d061b2cb65944f627daf061c80cf440d1324df95
Aleksander Fijołek <fijolek.olek@gmail.com>
2026-07-20 11:11:13 +0200

    feat: add geolocation calculations progress: finished first loop, during second loop

--- tasks/s01e02_findhim/solution.py
+++ tasks/s01e02_findhim/solution.py
@@ -1,5 +1,50 @@
 from __future__ import annotations
+from typing import Annotated
+from pydantic import BaseModel, Field
+import math
 
+class GeoPoint(BaseModel):
+    latitude: Annotated[float| None, Field(ge=-90, le=90)]
+    longitude: Annotated[float | None, Field(ge=-180, le=180)]
 
-def haversine_distance(target_1: tuple[float, float], target_2: tuple[float, float])->float | None:
-    pass
+    def distance_to(self, target:GeoPoint):
+        if self == target:
+            distance:float = 0.0
+        else:
+            # Promień Ziemi w kilometrach
+            R: float = 6356.4445
+
+            lat1, lon1 = math.radians(self.latitude), math.radians(self.longitude)
+            lat2, lon2 = math.radians(target.latitude), math.radians(target.longitude)
+
+            dlat = lat2 - lat1
+            dlon = lon2 - lon1
+
+            a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
+            c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+            distance = R * c
+        return distance
+
+class PowerPlant(BaseModel):
+    location_name: Annotated[str, Field(frozen=True)]
+    location: Annotated[GeoPoint| str | None, Field()]
+    uuid: Annotated[str,Field(frozen=True)]
+    power_level: Annotated[int,Field(frozen=True)]
+    active: Annotated[bool, Field(frozen=True, default=True)]
+
+    def nearest_suspect(self, suspect: Suspect):
+        """
+        Oblicza po kolei odległości pomiędzy daną elektrownią,
+        a wszystkimi GeoPoints z LocationHistory podejrzanego.
+        Zwraca tylko elektrownię, ktra uzyskała najniższą
+        odległość od podejrzanego i zwraca słownik z uuid elektrowni, imieniem, nazwiskiem podejrzanego i odległością
+        """
+        pass
+
+class Suspect(BaseModel):
+    name: Annotated[str,Field(frozen=True)]
+    surname: Annotated[str,Field(frozen=True)]
+    born: Annotated[int,Field(frozen=True)]
+    age: int|None|str
+    locations_history=Field(default_factory=list[GeoPoint])
+    access_lvl: Annotated[int, Field(True)]
--- tasks/s01e02_findhim/test_solution.py
+++ tasks/s01e02_findhim/test_solution.py
@@ -1,36 +1,74 @@
-from solution import haversine_distance
+import uuid
+import code
+from pydantic import Field, BaseModel
+from typing import Annotated
+from solution import GeoPoint, Suspect, PowerPlant
+
+
 
 # Współrzędne:
 #   latitude > 0 =>  North (N)
+#   latitude = 0 => Equator (EQ)
 #   latitude < 0 => South (S)
+#
 #   longitude > 0 => East (E)
+#   longitude = 0 => Prime Meridian (PM)
 #   longitude < 0 => West (W)
 
+# Testing data-set
+WARSAW = GeoPoint(latitude=52.2297, longitude=21.0122)
+KRAKOW = GeoPoint(latitude=50.0647, longitude=19.9450)
+KIJOW = GeoPoint(latitude=50.27, longitude=30.31)
+NEAR_GRUDZIADZ = GeoPoint(latitude=51.15, longitude=30.14)
+GRUDZIADZ = GeoPoint(latitude=51.16127, longitude=30.13095)
+LOCATION_A = GeoPoint(latitude=11.0, longitude=22.0)
+TARGET_PLANT= PowerPlant(location_name="Grudziądz", location=NEAR_GRUDZIADZ, uuid="PWR2764PL", power_level=9001, active=True )
+RANDOM_PLANT=PowerPlant(location_name="za lasami", location=LOCATION_A, uuid="abs123pl", power_level=12)
+EVIL_DUDE = Suspect(name="Maurycy", born=1990, surname="Dreptak", locations_history=[WARSAW, KRAKOW], access_lvl=2, age=None)
+
+### TDD CYCLE 01 ###
 
 
-def test_haversine_distance_same_location()->float | None:
+def test_GeoPoint_calc_distance_to()->float | None:
+    """
+    Testujemy metodę obliczającą odległość pomiędzy punktem A i B metodą haversine:
+    Łuk mierzony po powierzchni Ziemi od pkt A do pkt B
+    Wzór:
+    Standard case: Warszawa <-|251 km |-> Kraków
+    Maksymalny margines błędu ±1% (±2.5km)
+    """
+    distance:float = WARSAW.distance_to(KRAKOW)
+    assert 248.5 <= distance <= 253.5
+
+def test_GeoPoint_calc_distance_from_A_to_A():
     """ Test the haversine distance calculation. Edge case: same location """
-    power_plant: tuple[float, float] = (21.00, 52.00)
-    city: tuple[float, float] = (21.00, 52.00)
-    distance = haversine_distance(power_plant, city)
-    assert distance == 0.0
-    return distance
+    distance: float | None = WARSAW.distance_to(WARSAW)
+    assert distance == 0
 
-def test_haversine_distance_calculation()->float | None:
-    """ Test the haversine distance calculation. Standard case: Warszawa -> Kraków, ok. 251 km. """
-    warszawa: tuple[float, float] = (21.0122, 52.2297)
-    krakow: tuple[float, float] = (19.9450, 50.0647)
-    distance = haversine_distance(warszawa, krakow)
-    # Dystans podawany w km, tolerancja ±1% (linia prosta po powierzchni Ziemi)
-    assert distance is not None
-    assert 245 <= distance <= 257
-    return distance
+### TDD CYCLE 02 ###
 
-def test_haversine_distance_gt_zero()->float | None:
-    """ Test if the haversine distance calculation returns a value greater than zero. """
-    power_plant =(21.00, 52.00)
-    city = (52.00, 21.00)
-    distance = haversine_distance(power_plant, city)
-    assert distance is not None
-    assert distance > 0
-    return distance
+def test_calc_age():
+    """Poprawność obliczania wieku na podstawie roku urodzenia"""
+    test_dude = EVIL_DUDE.model_copy()
+    test_dude.calc_age()
+    assert EVIL_DUDE.age == 26
+
+def test_get_coordinats_to_city():
+
+
+def test_inspect_location_history():
+    near_suspect = GeoPoint(longitude=None,latitude=None)
+    assert TARGET_PLANT.nearest_suspect()
+
+
+def test_nearest_powerplant():
+    """
+    Czy podawana elektrownia jest najbliższa
+    """
+
+def test_answer_completion():
+    payload=Answer(
+        name=EVIL_DUDE.name,
+        surname=EVIL_DUDE.surname,
+        access_lvl=
+    )
